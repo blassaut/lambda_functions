ceea7a2922d5321db925a6511d0ac2cfbc92b41c
Benjamin Lassaut <benjamin.lassaut@gmail.com>
2020-03-23 10:23:24 +0100

    Add Lambda functions

---
+++ stopEC2instance.py
@@ -0,0 +1,9 @@
+import boto3
+region = 'eu-west-1'
+instances = ['i-xxx']
+
+def lambda_handler(event, context):
+    ec2 = boto3.client('ec2', region_name=region)
+    ec2.stop_instances(InstanceIds=instances)
+    print 'stopped your instances: ' + str(instances)
+
